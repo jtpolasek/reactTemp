d6f501edae90358573db3cb05efcec52a446f0b5
Todd <tpolasek@gmail.com>
2017-07-20 15:18:00 -0700

    broke out chart to its own component

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Sparklines, SparklinesLine} from 'react-sparklines'
+import { Sparklines, SparklinesLine} from 'react-sparklines';
 
 export default (props) =>{
-  <div>
-    <Sparklines svgHeight={120} svgWidth={180} data={props.data}>
-      <SparklinesLine color={props.color} />
-    </Sparklines>
-  </div>
-}
+    return (
+      <div>
+        <Sparklines svgHeight={120} svgWidth={180} data={props.data}>
+          <SparklinesLine color={props.color} />
+        </Sparklines>
+      </div>
+    )
+  }
